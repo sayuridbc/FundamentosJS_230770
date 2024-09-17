83844d88c20544a36f429d5fd3d02e25418b7b24
Sayurid Bautista Cruz <230770@utxicotepec.edu.mx>
2024-09-17 15:42:59 -0600

    Agregación de tipo de datos string, big, init, null

---
+++ JS/Ejercicio3.js
@@ -0,0 +1,37 @@
+// Strings o cadenas de texto 
+//const alumno = "Juan"
+//let producto = `Monitor 20 pulgadas` 
+
+//const numero = "30"
+//const numero2 =30 
+
+//console.log(typeof numero)
+//console.log(typeof numero2)
+
+//BigInt 
+
+//const numeroGrande= BigInt (41951565486648965549)
+//console.log(typeof numeroGrande)
+
+//const numero=10 
+//const numero2=20 
+//console.log( numero = Number (numeroGrande))
+
+//const numero = "30"
+//const numero2 = 30 
+
+//console.log(typeof String(numero2))
+//console.log(typeof Number(numero))
+
+//symbol 
+//const primerSymbol = Symbol(30)
+//const segundoSymbol = Symbol(30)
+
+//console.log( primerSymbol === segundoSymbol)
+
+//console.log( primerSymbol.valueOf()) 
+//console.log( segundoSymbol.valueOf()) 
+
+//null 
+//const descuento= null
+//console.log(typeof descuento) 
